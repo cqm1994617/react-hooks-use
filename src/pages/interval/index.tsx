d534c7ad326d886fdec53cc73c9a183d87c93cb0
cqm1994617 <1025760508@qq.com>
2020-06-30 23:17:40 +0800

    interval

--- src/pages/interval/index.tsx
+++ src/pages/interval/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function IntervalPage() {
 
+  const [data, setData] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setData(data => data + 1)
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
   return (
     <div>
-      interval
+      {data}
     </div>
   )
 }
