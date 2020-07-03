b5012f28f992c450cfc48e5a25514c9ef16854a0
cqm1994617 <1025760508@qq.com>
2020-07-03 23:40:14 +0800

    删除多余“

--- src/pages/closure/index.tsx
+++ src/pages/closure/index.tsx
@@ -1,15 +1,4 @@
-import React, { useState, useEffect, useReducer, useRef } from 'react'
-
-interface ActionType {
-  type?: string;
-}
-
-function reducer(state = 0, action: ActionType) {
-  switch (action.type) {
-    case 'SET':
-      return 10
-  }
-}
+import React, { useState, useEffect, useRef } from 'react'
 
 function useRefState(initValue: any) {
   const [state, setState] = useState(initValue)
@@ -30,7 +19,6 @@ function useRefState(initValue: any) {
 function ClosurePage() {
 
   const [data, setData] = useState(0)
-  const [state, dispatch] = useReducer(reducer, 0)
   const [value, valueRef, setValue] = useRefState(0)
 
   const testClick = () => {
@@ -39,17 +27,7 @@ function ClosurePage() {
       console.log(data)
     }, 0)
   }
-
   const testClick2 = () => {
-    dispatch({
-      type: 'SET'
-    })
-    setTimeout(() => {
-      console.log(state)
-    }, 0)
-  }
-
-  const testClick3 = () => {
     setValue(10)
     setTimeout(() => {
       console.log(value, valueRef.current)
@@ -59,8 +37,7 @@ function ClosurePage() {
   return (
     <div>
       <div onClick={testClick}>1 --> {data}</div>
-      <div onClick={testClick2}>2 --> {state}</div>
-      <div onClick={testClick3}>3 --> {value}</div>
+      <div onClick={testClick2}>2 --> {value}</div>
     </div>
   )
 }
