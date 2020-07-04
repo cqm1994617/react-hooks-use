441b6d75c8f02fc8b4b6bd6de8818e84f8885b6b
cqm1994617 <1025760508@qq.com>
2020-07-04 23:50:47 +0800

    'save'

--- src/pages/network/index.tsx
+++ src/pages/network/index.tsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+function mockRequest(url: string): Promise<object> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        url,
+        data: "返回数据"
+      })
+    }, 2000)
+  })
+}
+
+function useRequest(requestPromise: Promise<object>) {
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    requestPromise.then(res => {
+      
+    })
+  }, [])
+
+}
 
 function NetworkPage() {
+
+  useEffect(() => {
+    useRequest(mockRequest('url'))
+  }, [])
 
   return (
     <div>
