4a55e1b0931122f9f7065cdb3b8b045043db835a
cqm1994617 <1025760508@qq.com>
2020-07-05 22:03:08 +0800

    dispatch方式避免闭包陷阱

--- src/pages/closure/index.tsx
+++ src/pages/closure/index.tsx
@@ -1,4 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useReducer } from 'react'
+
+interface ActionType {
+  type?: string
+}
+
+interface StateType {
+  type?: string
+}
+
+const initalState = {
+  data: 0
+}
+
+function reducer(state = initalState, action: ActionType) {
+  switch(action.type) {
+    case 'ADD':
+      return {
+        data: state.data + 1
+      }
+    default:
+      return {
+        ...state
+      }
+  }
+}
 
 function useRefState(initValue: any) {
   const [state, setState] = useState(initValue)
@@ -19,25 +44,38 @@ function useRefState(initValue: any) {
 function ClosurePage() {
 
   const [data, setData] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {
+    data: 0
+  })
   const [value, valueRef, setValue] = useRefState(0)
 
   const testClick = () => {
-    setData(10)
+    setData(data + 1)
     setTimeout(() => {
       console.log(data)
     }, 0)
   }
   const testClick2 = () => {
-    setValue(10)
+    setValue(value + 1)
     setTimeout(() => {
       console.log(value, valueRef.current)
     }, 10)
+  }
+  const testClick3 = () => {
+
+    setTimeout(() => {
+      dispatch({
+        type: 'ADD'
+      })
+    }, 100)
+
   }
 
   return (
     <div>
       <div onClick={testClick}>1 --> {data}</div>
       <div onClick={testClick2}>2 --> {value}</div>
+      <div onClick={testClick3}>3 --> {state.data}</div>
     </div>
   )
 }
