0130f903f19df2116ebe80dcfce6f764e6e20f85
cqm1994617 <1025760508@qq.com>
2020-07-11 23:32:23 +0800

    存

--- src/pages/useState/index.tsx
+++ src/pages/useState/index.tsx
@@ -1,41 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-let state: any = []
-let setters: any = []
-let isFirst = true
-let cursor = 0
 
-function createSetter(cursor: number) {
-  return function (newValue: any) {
-    state[cursor] = newValue
-    ReactDOM.render(<UseStatePage />, document.getElementById('root'))
-  }
-}
-
-function useState(initValue: any) {
-  if (isFirst) {
-    state.push(initValue)
-    setters.push(createSetter(cursor))
-  }
-
-  const value = state[cursor]
-  const setter = setters[cursor]
-
-  return [
-    value,
-    setter
-  ]
-
-}
 
 function UseStatePage() {
 
   const [name, setName] = useState('Nick')
 
+  const click = (e:any) => {
+    console.log(e)
+    console.log(e.type)
+  }
+
   return (
     <div id="root">
-      <button onClick={() => setName('Nick-Update')}>{name}</button>
+      <button onClick={click}>{name}</button>
     </div>
   )
 }
